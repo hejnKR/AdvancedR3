d43bc88c99d3a74e46f948d1f284917faa440ca2
Kamilla Holtmann Hejn <hejn@bmb.sdu.dk>
2023-11-09 15:38:59 +0100

    metabolite_to_wider() and create_recipe_spec() created

--- R/functions.R
+++ R/functions.R
@@ -21,3 +21,44 @@ plot_distribution <- function(df) {
     ggplot2::geom_histogram() +
     ggplot2::facet_wrap(vars(metabolite), scales = "free")
 }
+
+#' column values to snake case
+#'
+#' @param df cols to make to snake case in df
+#'
+#' @return df
+#'
+column_values_to_snake_case <- function(df, cols) {
+  df %>%
+    dplyr::mutate(dplyr::across({{ cols }}, snakecase::to_snake_case))
+}
+
+#' pivot metabolite to wider
+#'
+#' @param df containing columns metabolite, value, and mean
+#'
+#' @return df wide format
+#'
+metabolites_to_wider <- function(df) {
+  df %>%
+    tidyr::pivot_wider(
+      names_from = metabolite,
+      values_from = value,
+      values_fn = mean,
+      names_prefix = "metabolite_"
+    )
+}
+
+#' Create recipe spec to pre-process data
+#'
+#' @param df wide df
+#' @param metabolite_variable column of metabolite of intrest
+#'
+#' @return returns recipe with specifications
+#'
+create_recipe_spec <- function(df, metabolite_variable) {
+  recipes::recipe(df) %>%
+    recipes::update_role({{ metabolite_variable }}, age, gender, new_role = "predictor") %>%
+    recipes::update_role(class, new_role = "outcome") %>%
+    recipes::step_normalize(tidyselect::starts_with("metabolite_"))
+}
