98b4ac38cde7f1e37d6e3d059e93eef20e549cee
Kamilla Holtmann Hejn <hejn@bmb.sdu.dk>
2023-11-09 10:57:00 +0100

    plot_distributions() added to functions.R

--- R/functions.R
+++ R/functions.R
@@ -9,3 +9,15 @@ descriptive_stats <- function(df) {
     dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
     dplyr::mutate(dplyr::across(dplyr::where(is.numeric), ~ round(.x, digits = 1)))
 }
+
+#' Distribution plot of metabolites
+#'
+#' @param df with value and metabolite column
+#'
+#' @return histograms for for metabolite values
+#'
+plot_distribution <- function(df) {
+  df %>% ggplot2::ggplot(aes(x = value)) +
+    ggplot2::geom_histogram() +
+    ggplot2::facet_wrap(vars(metabolite), scales = "free")
+}
