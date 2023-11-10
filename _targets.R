4045164c2c30b2639a21c5a3f9d6bff7947feab9
Kamilla Holtmann Hejn <hejn@bmb.sdu.dk>
2023-11-10 13:58:16 +0100

    connecting html to github

--- _targets.R
+++ _targets.R
@@ -67,8 +67,8 @@ list(
     command = plot_distribution(lipidomics)
   ),
   tar_quarto(
-    name = quarto_doc,
-    path = "doc/learning.qmd"
+    name = quarto_website,
+    path = "."
   ),
   tar_target(
     name = df_model_estimates,
